91de823d3601c96845e4975f0162c4ac334e8b88
sacrilegioushero220 <sandweepsabu@gmail.com>
2023-10-17 07:59:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learners_choice_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/learners_choice_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learners_choice_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
