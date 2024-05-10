bdd572778669283c8ab521813b7e7f0f1076235f
sacrilegioushero220 <sandweepsabu@gmail.com>
2024-05-10 20:06:41 +0530

    fixed as const in fab button

--- lib/core/widgets/customButtons/custom_fab_button.dart
+++ lib/core/widgets/customButtons/custom_fab_button.dart
@@ -1,55 +1,7 @@
 import 'package:flutter/material.dart';
 
-Widget buildCustomFabButton({
-  bool ishidden = false,
-  required context,
-  required String label,
-  required bool isReversed,
-  required void Function() onPressed,
-  IconData icon = Icons.arrow_forward,
-}) {
-  return FloatingActionButton.extended(
-    shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(40))),
-    backgroundColor: const Color(0xFFFCF1F3),
-    onPressed: onPressed,
-    label: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        isReversed
-            ? !ishidden
-                ? Icon(
-                    icon,
-                    color: const Color(0xFF00639B),
-                  )
-                : const SizedBox(width: 2)
-            : const SizedBox(width: 2),
-        Text(
-          label,
-          style: const TextStyle(
-            color: Color(0xFF00639B),
-            fontSize: 16,
-            fontFamily: 'Roboto',
-            fontWeight: FontWeight.w500,
-            height: 0.10,
-            letterSpacing: 0.10,
-          ),
-        ),
-        isReversed
-            ? const SizedBox(width: 8)
-            : !ishidden
-                ? const Icon(
-                    Icons.arrow_forward,
-                    color: Color(0xFF00639B),
-                  )
-                : const SizedBox(width: 2),
-      ],
-    ),
-  );
-}
-
 class BuildCustomFabButton extends StatelessWidget {
-  BuildCustomFabButton({
+  const BuildCustomFabButton({
     required this.heroTag,
     super.key,
     this.icon = Icons.arrow_forward,
@@ -59,12 +11,12 @@ class BuildCustomFabButton extends StatelessWidget {
     required this.onPressed,
   });
 
-  bool ishidden;
+  final bool ishidden;
   final String heroTag;
   final String label;
   final bool isReversed;
   final void Function() onPressed;
-  IconData icon;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
