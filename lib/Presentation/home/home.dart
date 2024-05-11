cdb202b65c1ce3b60d9a416c1902e899d4dac4e7
sacrilegioushero220 <sandweepsabu@gmail.com>
2024-05-11 15:11:04 +0530

    Back button feature is implemented

--- lib/Presentation/home/home.dart
+++ lib/Presentation/home/home.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:learners_choice_app/core/blocs/cubit/cubit/local_storage_cubit.dart';
 import 'package:learners_choice_app/core/extensions/color_extention.dart';
 import 'package:learners_choice_app/presentation/home/docs_screen.dart';
 import 'package:learners_choice_app/presentation/home/home_screen.dart';
@@ -21,38 +21,59 @@ class _HomeState extends State<Home> {
     const InfoScreen(),
   ];
   int _selectedIndex = 0;
-
+  bool canPop = false;
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  bool _canPop() {
+    if (_selectedIndex > 0) {
+      return false;
+    } else if (_selectedIndex == 0) {
+      return true;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.onPrimary,
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: context.surfaceVariant,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
+    return PopScope(
+      canPop: _canPop(),
+      onPopInvoked: (canPop) {
+        if (_selectedIndex > 0) {
+          setState(() {
+            _selectedIndex = 0;
+          });
+        } else if (_selectedIndex == 0) {
+          exit(0);
+        }
+      },
+      child: Scaffold(
+        backgroundColor: context.onPrimary,
+        body: screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: context.surfaceVariant,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+              ),
+              label: "Home",
             ),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.description_outlined),
-            label: "Docs",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info_outlined),
-            label: "info",
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.description_outlined),
+              label: "Docs",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info_outlined),
+              label: "info",
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
