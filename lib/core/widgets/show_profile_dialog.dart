836ddda63faf49173633c4c8f2b9e2f4916cdf77
sacrilegioushero220 <sandweepsabu@gmail.com>
2024-05-12 18:47:08 +0530

    handled the null case in the textfield of the profile dialog

--- lib/core/widgets/show_profile_dialog.dart
+++ lib/core/widgets/show_profile_dialog.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learners_choice_app/core/blocs/cubit/cubit/local_storage_cubit.dart';
+import 'package:learners_choice_app/core/extensions/text_extension.dart';
 import 'package:learners_choice_app/core/widgets/my_custom_widgets.dart';
 
 Future<dynamic> showProfileDialog(
   BuildContext context,
 ) async {
   return showModalBottomSheet(
-      context: context, builder: (context) => const CustomBottomSheet());
+    isScrollControlled: true,
+    showDragHandle: true,
+    context: context,
+    builder: (context) => const CustomBottomSheet(),
+  );
 }
 
 // ignore: must_be_immutable
@@ -21,132 +26,152 @@ class CustomBottomSheet extends StatelessWidget {
     String profilePic = "";
     String profileName = "";
     final controller = TextEditingController();
-    return SafeArea(
-      child: ListView(
-        padding: const EdgeInsets.all(16),
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        children: [
-          const ListTile(
-            title: Text("Profile"),
-          ),
-          Row(
+    return SingleChildScrollView(
+      child: Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            //padding: const EdgeInsets.all(16),
+            //keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
             children: [
-              BlocConsumer<LocalStorageCubit, LocalStorageState>(
-                listener: (context, state) {
-                  if (state is ProfileUpdatedState) {
-                    profilePic = state.profilePic;
-                  } else if (state is ImagePicked) {
-                    profilePic = state.imagePath;
-                  }
-                },
-                builder: (context, state) {
-                  if (state is ProfileFetchedState) {
-                    profilePic = state.profilePic;
-                    return InkWell(
-                      onTap: () {
-                        BlocProvider.of<LocalStorageCubit>(context)
-                            .pickAndSaveImage();
-                      },
-                      child: BuildImageWidget(
-                        isCircleAvatar: true,
-                        isFileImage: true,
-                        boxFit: BoxFit.cover,
-                        width: 116,
-                        height: 163.39,
-                        imagePath: profilePic,
-                      ),
-                    );
-                  } else if (state is ProfileUpdatedState) {
-                    profilePic = state.profilePic;
-                    return InkWell(
-                      onTap: () {
-                        BlocProvider.of<LocalStorageCubit>(context)
-                            .pickAndSaveImage();
-                      },
-                      child: BuildImageWidget(
-                        isCircleAvatar: true,
-                        isFileImage: true,
-                        boxFit: BoxFit.cover,
-                        width: 116,
-                        height: 163.39,
-                        imagePath: profilePic,
-                      ),
-                    );
-                  }
-
-                  return InkWell(
-                    onTap: () {
-                      BlocProvider.of<LocalStorageCubit>(context)
-                          .pickAndSaveImage();
-                    },
-                    child: BuildImageWidget(
-                      isCircleAvatar: true,
-                      isFileImage: true,
-                      width: 100,
-                      height: 163.39,
-                      imagePath: profilePic,
-                      boxFit: BoxFit.fill,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Profile",
+                    style: context.captionLight?.copyWith(
+                      fontSize: 20,
                     ),
-                  );
-                },
+                  ),
+                ],
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: BlocConsumer<LocalStorageCubit, LocalStorageState>(
+              Row(
+                children: [
+                  BlocConsumer<LocalStorageCubit, LocalStorageState>(
                     listener: (context, state) {
-                      // TODO: implement listener
+                      if (state is ProfileUpdatedState) {
+                        profilePic = state.profilePic;
+                      } else if (state is ImagePicked) {
+                        profilePic = state.imagePath;
+                      }
                     },
                     builder: (context, state) {
                       if (state is ProfileFetchedState) {
-                        print(state.profileName);
-                        return NameTextField(
-                          controller: controller,
-                          hintText: state.profileName,
+                        profilePic = state.profilePic;
+                        return InkWell(
+                          onTap: () {
+                            BlocProvider.of<LocalStorageCubit>(context)
+                                .pickAndSaveImage();
+                          },
+                          child: BuildImageWidget(
+                            isCircleAvatar: true,
+                            isFileImage: true,
+                            boxFit: BoxFit.cover,
+                            width: 50,
+                            height: 60,
+                            imagePath: profilePic,
+                          ),
                         );
-                      }
-                      if (state is ProfileUpdatedState) {
-                        return NameTextField(
-                          controller: controller,
-                          hintText: state.profileName,
+                      } else if (state is ProfileUpdatedState) {
+                        profilePic = state.profilePic;
+                        return InkWell(
+                          onTap: () {
+                            BlocProvider.of<LocalStorageCubit>(context)
+                                .pickAndSaveImage();
+                          },
+                          child: BuildImageWidget(
+                            isCircleAvatar: true,
+                            isFileImage: true,
+                            boxFit: BoxFit.cover,
+                            width: 50,
+                            height: 60,
+                            imagePath: profilePic,
+                          ),
                         );
                       }
 
-                      return NameTextField(
-                        controller: controller,
-                        hintText: profileName,
+                      return InkWell(
+                        onTap: () {
+                          BlocProvider.of<LocalStorageCubit>(context)
+                              .pickAndSaveImage();
+                        },
+                        child: BuildImageWidget(
+                          isCircleAvatar: true,
+                          isFileImage: true,
+                          width: 100,
+                          height: 163.39,
+                          imagePath: profilePic,
+                          boxFit: BoxFit.fill,
+                        ),
                       );
                     },
                   ),
-                ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: BlocConsumer<LocalStorageCubit, LocalStorageState>(
+                        listener: (context, state) {},
+                        builder: (context, state) {
+                          if (state is ProfileFetchedState) {
+                            print(state.profileName);
+                            return NameTextField(
+                              controller: controller,
+                              hintText: state.profileName,
+                            );
+                          }
+                          if (state is ProfileUpdatedState) {
+                            return NameTextField(
+                              controller: controller,
+                              hintText: state.profileName,
+                            );
+                          }
+
+                          return NameTextField(
+                            controller: controller,
+                            hintText: profileName,
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FilledButton(
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                    ),
+                    onPressed: () {
+                      final name = controller.text.trim();
+
+                      if (name.isNotEmpty) {
+                        BlocProvider.of<LocalStorageCubit>(context)
+                            .updateProfile(name);
+
+                        Navigator.pop(context);
+                      } else {
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Icon(Icons.done),
+                  ),
+                ],
               ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 12,
-                ),
-              ),
-              onPressed: () {
-                final name = controller.text.trim();
-
-                BlocProvider.of<LocalStorageCubit>(context).updateProfile(name);
-
-                print(profileName);
-              },
-              child: const Text("Update"),
-            ),
-          ),
-          const SizedBox(height: 10),
-        ],
+        ),
       ),
     );
   }
