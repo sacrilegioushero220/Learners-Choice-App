52eaeb5d10e2751b900d0c357e3423a6ba54a81f
Abhiram T Sabu <sandweepsabu@gmail.com>
2024-07-03 23:04:25 +0530

    quiz loading from json feature is started

--- lib/model/quiz_question.dart
+++ lib/model/quiz_question.dart
@@ -2,21 +2,23 @@ class QuizQuestion {
   final String question;
   final List<String> options;
   final int correctAnswerIndex;
-  final String? imageUrl;
+  final String imageUrl;
 
   QuizQuestion({
     required this.question,
     required this.options,
     required this.correctAnswerIndex,
-    this.imageUrl,
+    required this.imageUrl,
   });
 
   factory QuizQuestion.fromMap(Map<String, dynamic> map) {
     return QuizQuestion(
-      question: map['question'] ?? '',
+      question: map['question'] as String,
       options: List<String>.from(map['options']),
-      correctAnswerIndex: map['correctAnswerIndex'] as int,
-      imageUrl: map['imageUrl'],
+      correctAnswerIndex: (map['correctAnswerIndex'] is int)
+          ? map['correctAnswerIndex'] as int
+          : int.tryParse(map['correctAnswerIndex'].toString()) ?? 0,
+      imageUrl: map['imageUrl'] as String,
     );
   }
 
